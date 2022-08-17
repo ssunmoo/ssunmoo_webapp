458d27e295850d058df724a847ebc72f0cd995d3
HY <HY@DESKTOP-M2RRMUQ>
2022-08-18 00:17:50 +0900

    8.18 h

--- front2/public/study/엘레베이터.js
+++ front2/public/study/엘레베이터.js
@@ -9,59 +9,76 @@ elevator algorithm
 
 엘리베이터 위치와 내위치가 달랐을때
 상승 하강 눌렀을때 만나도록
-
-
  */
- 
-let output = []
-// 층수를 출력할때 배열로 출력할래
 
-/*
 
-// btn_up 을 눌렀을때 층수 하나씩 올라가야하니까 1개씩 더해지게 해줘.....
-// 숫자니까 배열로 만들고싶다..
 
-	for(let i = 1; i <= 10; i++){
-		
-		btn_up += i
-		
-	document.getElementById('output').innerHTML = btn_up;	
+
+let 내위치 = Number(prompt('내위치 입력:'))
+let 현재엘베층수 = Number(prompt('엘베위치 입력:'))
+
+function 상승(){	// 상승 버튼을 눌렀을때 층수가 1개씩 올라간다
+	// 상승버튼을 눌렀을때 엘베위치 값이 내 위치 값까지 내려올때까지 반복
+	if( 현재엘베층수 > 내위치 ){
+		현재엘베층수--
+	}else{
+		return
 	}
-
-
-// btn_down 을 눌렀을때 1개씩 빼지게 해줘.....
-	for(let j = 10; j <= 1; i--){
-		
-		btn_down += j
-	}
-
-
-*/
-
- 
- 
- 
- function btn_print(){
-	
-	let btnlist = '' // 엘리베이터 층수인 1부터 10층까지의 버튼을 저장할 변수 선언
-		for(let i = 1; i <= 10; i++){
-			// 1부터 10까지 1씩 증가해서 만들어줘
-		btnlist += `<button id=${i} onclick=btn_click(${i})> ${i} </button>`
-		if(i % 2 == 0){
-			btnlist += '<br>'
-		}
-		}
-		document.getElementById('btnbox').innerHTML = btnlist;
+		document.getElementById('엘베위치').innerHTML = 현재엘베층수;
+		document.getElementById('내위치').innerHTML = 내위치;	
 }
 
+function 하강(){	// 하강 버튼을 눌렀을때 층수가 1개씩 내려간다
+	if(현재엘베층수 < 내위치){
+		현재엘베층수++	
+	}else{
+		return
+	}
+	document.getElementById('엘베위치').innerHTML = 현재엘베층수;
+	document.getElementById('내위치').innerHTML = 내위치;
+}
+/*
 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+if(현재엘베층수 < 내위치){
+	for(i = 현재엘베층수; i < 내위치; i++){
+		setTimeout(상승(), 1 * 1000)}
+	}
+*/	
+
+
+// + 온클릭을 눌렀을때 if값을 밖으로 빼서 그안에 펑션이 작동하게?
+
+
+
+// 현재엘베층수 < 내위치가 낮으면 현재엘베층수가 내려가
+// 현재엘베층수 > 내위치가 높으면 현재엘베층수가 올라가
+	
+	
+	
+	
+	
+	
+/*
+
+function 상승(){	// 상승 버튼을 눌렀을때 층수가 1개씩 올라간다
+	if( 층수 < 10 ){
+		층수++
+	}else{
+		return
+	}
+	document.getElementById('엘베위치').innerHTML = 층수;	
+	document.getElementById('내위치').innerHTML = 층수;
+}
+
+function 하강(){	// 하강 버튼을 눌렀을때 층수가 1개씩 내려간다
+	if(층수 > 1){
+		층수--	
+	}else{
+		return
+	}
+	
+	document.getElementById('엘베위치').innerHTML = 층수;
+	document.getElementById('내위치').innerHTML = 층수;
+}
+
+*/
